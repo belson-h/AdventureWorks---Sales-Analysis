c0bfaf1c8ac76860cadbc08d846f3e7dba817311
Isabel Holm <isabel.c.holm@gmail.com>
2025-12-17 12:42:04 +0100

    Reapply "creating project setup"
    This reverts commit dd1416ca9357b903bf4537047a3031f18e59f549.

---
+++ data/vis1.sql
@@ -0,0 +1,19 @@
+--Skapa SQL queries här för första visualiseringen, skapa sedan en ny SQL fil för varje ny visualisering
+
+USE AdventureWorks2025
+
+
+SELECT * FROM Production.ProductCategory
+SELECT * FROM Production.ProductSubcategory
+SELECT * FROM Production.Product
+
+
+SELECT
+    pc.Name AS CategoryName,
+    COUNT(DISTINCT p.ProductID) AS ProductCount
+
+FROM Production.ProductCategory pc
+INNER JOIN Production.ProductSubcategory psc ON pc.ProductCategoryID = psc.ProductCategoryID
+INNER JOIN Production.Product p ON psc.ProductSubcategoryID = p.ProductSubcategoryID
+GROUP BY pc.Name
+ORDER BY ProductCount DESC
